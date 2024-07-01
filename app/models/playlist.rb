40e0a1fdb90997d1b264590a7f12978eb75e38b0
Jill <jho3292@gmail.com>
2024-07-01 18:52:40 -0400

    Add images to playlists.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,7 @@
 class Playlist < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
