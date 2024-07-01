0e49c4ca63c8af4f7fdd5e699b24b7019c8545e6
Jill <jho3292@gmail.com>
2024-06-30 20:41:10 -0400

    Remove typo on devise config file.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,4 @@
-config.navigational_formats = ['*/*', :html, :turbo_stream]# frozen_string_literal: true
+# frozen_string_literal: true
 
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
