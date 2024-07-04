a582853dab027209b9d096d22180952dde45f496
Jill <jho3292@gmail.com>
2024-07-04 15:24:07 -0400

    Add short tracklist and user info to playlists index page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :playlists, :only => [:index, :show, :new, :create, :edit, :update]
   devise_for :users
 
-  get '/playlists/:id/tracks', to: 'tracks#show'
+  get '/playlists/:id/tracks', to: 'tracks#show', as: :view_playlist
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
