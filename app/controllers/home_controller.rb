dc3c5736e551c309c851851699e3d57e5044fde2
Jill <jho3292@gmail.com>
2024-07-04 17:45:07 -0400

    Create user profile page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
     def index
         render
     end
+
+    def account
+        @user = User.find(params[:id])
+        @playlists = @user.playlists
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :playlists, :only => [:index, :show, :new, :create, :edit, :update]
   devise_for :users
-
+  
   get '/playlists/:id/tracks', to: 'tracks#show', as: :view_playlist
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   root to: "home#index"
+  get 'users/:id', to: 'home#account', as: :user_account
 
   # Defines route to search
   get '/playlists/:id/tracks/new', to: 'tracks#new', as: :search_tracks
