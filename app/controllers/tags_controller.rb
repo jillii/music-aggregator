6bd2553fb753f97c7b49a97cef76689c4de66bda
Jill <jho3292@gmail.com>
2024-07-10 09:19:17 -0400

    Remove unecessary tag stuff.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,25 +1,4 @@
 class TagsController < ApplicationController
-  # GET /playlist/:id/update
-  def new
-    @tag = Tag.new
-  end
-
-  # POST /playlist/:id/update
-  def create
-    @tag = Tag.new(tag_params)
-    @tag.playlist_id = params[:id]
-
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to playlist_path(@tag), notice: "Playlist was successfully created." }
-        format.json { render :show, status: :created, location: @tag }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /playlists/1 or /playlists/1.json
   def destroy
     @tag = Tag.find(params[:id])
@@ -30,11 +9,4 @@ class TagsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
-  private
-
-    # Only allow a list of trusted parameters through.
-    def tag_params
-      params.require(:tag).permit(:label)
-    end
 end
