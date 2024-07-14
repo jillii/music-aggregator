2889d7e6253ff50039cb77e5655c48463b55c4bf
Jill <jho3292@gmail.com>
2024-07-14 15:42:18 -0400

    Add editing playlists to user account page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,6 @@ class HomeController < ApplicationController
     def account
         @user = User.find(params[:id])
         @playlists = @user.playlists
+        @editor_playlists = @user.playlists_editing
     end
 end
