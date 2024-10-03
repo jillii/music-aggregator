777b5077edaf00cd81c6a75f92a2f56a53a4da43
Jill <jho3292@gmail.com>
2024-10-02 23:12:21 -0400

    Make range slide work.

--- app/assets/javascript/youtubePlayer.js
+++ app/assets/javascript/youtubePlayer.js
@@ -12,6 +12,7 @@ reloadYoutube();
 
 let player;
 let is_looping = false;
+const range = document.getElementById('range');
 
 addEventListener('turbo:load', () => {
     // check if playlist already exists
@@ -98,6 +99,19 @@ addEventListener('turbo:load', () => {
         } else {
             document.title = 'Greatdj3';
         }
+        // Update the controls on load
+        updateTimerDisplay();
+        updateProgressBar();
+
+        // Clear any old interval.
+        // clearInterval(time_update_interval);
+
+        // Start interval to update elapsed time display and
+        // the elapsed part of the progress bar every second.
+        time_update_interval = setInterval(function () {
+            updateTimerDisplay();
+            updateProgressBar();
+        }, 1000)
     }
 });
 
@@ -143,4 +157,33 @@ function youtube_player () {
             play.innerHTML = "Pause";
         });
     }
+}
+
+function updateTimerDisplay() {
+    // Update current time text display.
+    document.getElementById('current-time').innerHTML = formatTime( player.getCurrentTime() );
+}
+function updateProgressBar() {
+    // Update the value of our progress bar accordingly.
+    range.value = (player.getCurrentTime() / player.getDuration()) * 100;
+}
+
+function formatTime(time){
+    time = Math.round(time);
+
+    var minutes = Math.floor(time / 60),
+    seconds = time - minutes * 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return minutes + ":" + seconds;
+}
+
+range.addEventListener('changemouseup touchend', updateTime, false);
+range.addEventListener('touchend', updateTime, false);
+
+function updateTime (e) {
+    // Calculate the new time for the video.
+    // new time in seconds = total duration in seconds * ( value of range input / 100 )
+    var newTime = player.getDuration() * (e.target.value / 100);
+    // Skip video to new time.
+    player.seekTo(newTime);
 }
