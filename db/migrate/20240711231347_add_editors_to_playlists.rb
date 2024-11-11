d9f85418aeff76860cd7e3611cc43a794b2459ae
Jill <jho3292@gmail.com>
2024-11-11 14:58:02 -0500

    Edit addEditorsPlaylists migration to use sigular form.

--- db/migrate/20240711231347_add_editors_to_playlists.rb
+++ db/migrate/20240711231347_add_editors_to_playlists.rb
@@ -1,7 +1,7 @@
 class AddEditorsToPlaylists < ActiveRecord::Migration[7.0]
   def change
     create_table :editors_playlists do |t|
-      t.references :playlists, null: false, foreign_key: true
+      t.references :playlist, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
     end
 
