3f012c448f7577b328759e5a913d3c03204c88cf
Jill <jho3292@gmail.com>
2024-11-16 20:45:13 -0500

    Configure resend mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,17 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # change before going live
-  config.action_mailer.default_url_options = { host: 'playlistswithfriends.app' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'playlistswithfriends.app',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "playlistswithfriends.app", protocol:"https" }
+  config.action_mailer.delivery_method = :resend
   # Code is not reloaded between requests.
   config.cache_classes = true
 
