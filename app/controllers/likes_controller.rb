91e2a5deabf6482414a38f777485652da8a19f9a
Jill <jho3292@gmail.com>
2024-11-20 17:40:01 -0500

    Create notification when user likes playlist.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,15 @@ class LikesController < ApplicationController
     @playlist = Playlist.find(params[:playlist_id])
     current_user.likes.create!(playlist: @playlist)
 
+    notification = Notification.new(
+      message: "<a href='/users/#{current_user.id}'>#{current_user.username ? current_user.username : current_user.email} likes #{@playlist.title}</a>",
+      sender_id: current_user.id,
+      recipient_id: @playlist.user_id,
+      read: false
+    )
+
     respond_to do |format|
+      notification.save
       format.html { redirect_to playlist_path(@playlist)}
     end
   end
