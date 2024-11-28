301679ea8894ac29a758c4e394b1beea259cb6cf
Jill <jho3292@gmail.com>
2024-11-27 23:37:02 -0500

    Reload page when user hits back button.

--- app/assets/javascript/pageReloadHandler.js
+++ app/assets/javascript/pageReloadHandler.js
@@ -5,3 +5,8 @@ addEventListener('turbo:click', (e) => {
 addEventListener('turbo:submit-end', (e) => {
     history.pushState({}, null, e.detail.fetchResponse.response.url);
 })
+
+// Reload page if user clicks browser back button
+addEventListener("popstate", (event) => {
+    Turbo.visit(window.location.href, { action: "replace" });
+});
