5d716ba27eabe8572225ca9b47ccfb1f190f9d6e
Jill <jho3292@gmail.com>
2025-05-30 22:09:44 -0400

    Remove tag from playlist upon delete (don't destroy tag).

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,17 @@
 class TagsController < ApplicationController
   # DELETE /playlists/1 or /playlists/1.json
   def destroy
-    @tag = Tag.find(params[:id])
-    @tag.destroy
+    playlist = Playlist.find(params[:playlist_id])
+    tag = Tag.find(params[:id])
+
+    playlist.tags.delete(tag)
+    tag.destroy if tag.playlist_ids.empty?
 
     respond_to do |format|
-      format.html { redirect_to request.referrer, notice: "Tag was successfully removed." }
-      format.json { head :no_content }
+      if playlist.save
+        format.html { redirect_to request.referrer, notice: "Tag was successfully removed." }
+        format.json { head :no_content }
+      end
     end
   end
 end
