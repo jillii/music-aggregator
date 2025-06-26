9fc02aac02857649896c7871351750972dd25f17
Jill <jho3292@gmail.com>
2025-06-26 17:00:36 -0400

    Style notifications.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :recipient, class_name: 'User', foreign_key: 'recipient_id'
-  paginates_per 6
+  paginates_per 13
 end
